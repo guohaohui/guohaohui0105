0d0e6b32519b7e9cf3a819145f23c48981f4131b
guohaohui <2409734992@qq.com>
2021-03-02 15:04:14 +0800

    update

--- ds/Singly linked lists/linkList.h
+++ ds/Singly linked lists/linkList.h
@@ -1,19 +1,20 @@
 #ifndef __LINKLIST_H
 #define __LINKLIST_H
 
-
-typedef struct data
+//  数据结点
+typedef struct nose_st
 {
-    void *data;
-    struct data *next;
-};
+    void *data; //  数据域首地址
+    struct node_st *next;   //指向下一个数据结点的地址
+}node_t;
 
-typedef struct head
+//  头节点
+typedef struct 
 {
-    struct data *head;
+    struct node_st *next;   //指向下一个结点（下一个结点是struct node_st 类型的）
     int size;
     int nmemb;
-};
+}listhead_t;
 
 
 #endif
